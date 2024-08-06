1deb18e3b4a371b4ea5200603cbaa58d475089ed
Benojir Sultana <124562791+Benojir@users.noreply.github.com>
2024-08-06 11:37:57 +0530

    Update AudioPlayerDialog.java

--- app/src/main/java/zoro/benojir/callrecorder/dialogs/AudioPlayerDialog.java
+++ app/src/main/java/zoro/benojir/callrecorder/dialogs/AudioPlayerDialog.java
@@ -5,7 +5,6 @@ import android.app.Dialog;
 import android.net.Uri;
 import android.util.Log;
 import android.view.MotionEvent;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageButton;
